44084c922649c049ed9afe9bae2a71c84a002e73
Chloe Brett <cbre0013@student.monash.edu>
2022-08-11 21:07:05 +1000

    Import config

--- backend-database/db.js
+++ backend-database/db.js
@@ -1,5 +1,6 @@
 // Setting up the database connection
-const knex = require("knex")();
+const dbConfig = require('knexfile.js');
+const knex = require("knex")(dbConfig.development);
 const bookshelf = require("bookshelf")(knex);
 
 // Defining models
