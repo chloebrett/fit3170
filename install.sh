574dd3f683c060b5d9d955f7b909f03b92539a71
Chloe Brett <cbre0013@student.monash.edu>
2022-08-11 23:28:42 +1000

    load nvm inline

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install 16
 
+# Load nvm immediately
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # install yarn
 npm i -g yarn
 
